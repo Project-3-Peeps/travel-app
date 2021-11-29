696233d3fba37610b6a20a4cffdb71be65e579a8
Chloe Harris <chloe.a.harris17@gmail.com>
2021-11-29 12:37:24 -0800

    fixing create itinerary errors

--- src/components/pages/itinerary/CreateItinerary.js
+++ src/components/pages/itinerary/CreateItinerary.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
-import "./ItineraryForm.css";
+import "./CreateItinerary.css";
 
 function ItineraryForm() {
   // Create state variables for the fields in the form
@@ -56,100 +56,94 @@ function ItineraryForm() {
   //   console.log(event.target);
   //   setDays(days++);
   //   console.log(days);
+
+  return (
+    // Create a itinerary view component with stars / rating logic?
+    // const [stars,setStars]= useState(0);
+    // const ratingClick = event => {
+    // console.log(event.target)
+    // }
+
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // const [day,setDay] = useState(1)
+
+    // <div>
+    //   users.map((itinerary) => (
+
+    //   ))
+    // </div>
+    <div className="container">
+      <p className="form-header">Create Your Triptinerary</p>
+      <Form>
+        <Form.Group className="mb-3">
+          <Form.Label htmlFor="title">Title</Form.Label>
+          <Form.Control
+            value={title}
+            name="title"
+            onChange={handleInputChange}
+            type="text"
+            placeholder="Title"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label htmlFor="summary">Trip Description</Form.Label>
+          <Form.Control
+            value={description}
+            name="description"
+            onChange={handleInputChange}
+            type="text"
+            as="textarea"
+            placeholder="Visible to all users"
+            rows={3}
+          />
+        </Form.Group>
+        <hr />
+        <Form.Group className=" text-bold mb-3">
+          <Form.Label htmlFor="description" name="days">
+            {`Day ${days}`}
+          </Form.Label>
+          <Form.Control name="days" rows={1} />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Activities</Form.Label>
+          <Form.Control
+            value={description}
+            name="activities"
+            onChange={handleInputChange}
+            type="text"
+            as="textarea"
+            placeholder="Activities, places to see, restaurants..."
+            rows={5}
+          />
+        </Form.Group>
+        <Form.Label htmlFor="price">Price</Form.Label>
+        <Form.Group className="input-group mb-3">
+          <div className="input-group-prepend">
+            <span className="input-group-text">$</span>
+          </div>
+          <Form.Control
+            value={price}
+            name="price"
+            onChange={handleInputChange}
+            type="text"
+            placeholder="Amount"
+            rows={3}
+          />
+        </Form.Group>
+        <Form.Group>
+          <Button className="btn-add rounded-pill" variant="success" size="md">
+            +
+          </Button>
+          <Button
+            className="btn-submit rounded-pill"
+            size="md"
+            onClick={handleFormSubmit}
+          >
+            Submit
+          </Button>
+        </Form.Group>
+      </Form>
+    </div>
+  );
 }
-
-return (
-  // Create a itinerary view component with stars / rating logic?
-  // const [stars,setStars]= useState(0);
-  // const ratingClick = event => {
-  // console.log(event.target)
-  // }
-
-  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  // const [day,setDay] = useState(1)
-
-  // <div>
-  //   users.map((itinerary) => (
-
-  //   ))
-  // </div>
-  <div className="container">
-    <p className="form-header">Create Your Triptinerary</p>
-    <Form>
-      <Form.Group className="mb-3">
-        <Form.Label htmlFor="title">Title</Form.Label>
-        <Form.Control
-          value={title}
-          name="title"
-          onChange={handleInputChange}
-          type="text"
-          placeholder="Title"
-        />
-      </Form.Group>
-      <Form.Group className="mb-3">
-        <Form.Label htmlFor="summary">Trip Description</Form.Label>
-        <Form.Control
-          value={summary}
-          name="description"
-          onChange={handleInputChange}
-          type="text"
-          as="textarea"
-          placeholder="Visible to all users"
-          rows={3}
-        />
-      </Form.Group>
-      <hr />
-      <Form.Group className=" text-bold mb-3">
-        <Form.Label htmlFor="description" name="days">
-          {`Day ${days}`}
-        </Form.Label>
-        <Form.Control name="days" rows={1} />
-      </Form.Group>
-      <Form.Group className="mb-3">
-        <Form.Label>Activities</Form.Label>
-        <Form.Control
-          value={description}
-          name="activities"
-          onChange={handleInputChange}
-          type="text"
-          as="textarea"
-          placeholder="Activities, places to see, restaurants..."
-          rows={5}
-        />
-      </Form.Group>
-      <Form.Label htmlFor="price">Price</Form.Label>
-      <Form.Group className="input-group mb-3">
-        <div className="input-group-prepend">
-          <span className="input-group-text">$</span>
-        </div>
-        <Form.Control
-          value={price}
-          name="price"
-          onChange={handleInputChange}
-          type="text"
-          placeholder="Amount"
-          rows={3}
-        />
-      </Form.Group>
-      <Form.Group>
-        <Button
-          className="btn-add rounded-pill"
-          variant="success"
-          size="md"
-          onClick={addDaysClick}
-        >
-          +
-        </Button>
-        <Button
-          className="btn-submit rounded-pill"
-          size="md"
-          onClick={handleFormSubmit}
-        >
-          Submit
-        </Button>
-      </Form.Group>
-    </Form>
-  </div>
-);
-
 export default ItineraryForm;
